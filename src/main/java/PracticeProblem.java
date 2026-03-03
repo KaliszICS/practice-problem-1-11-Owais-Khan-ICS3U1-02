3789b956d48e0cbf6292b177f6361c8ab70ae308
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-03 15:28:41 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -5,23 +6,50 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num = input.nextDouble();
+        System.out.println(Math.abs(num));
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Input another number: ");
+        double num2 = input.nextDouble();
+        double result = num1/num2;
+        System.out.println(Math.floor(result) + "\n" + Math.ceil(result));
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num3 = input.nextDouble();
+        System.out.println(Math.round(Math.sqrt(num3)));
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num4 = input.nextDouble();
+        System.out.print("Input another number: ");
+        double num5 = input.nextDouble();
+        System.out.println(Math.pow(num4, num5));
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner input = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double num6 = input.nextDouble();
+        System.out.print("Input another number: ");
+        double num7 = input.nextDouble();
+        System.out.print("Input one more number: ");
+        double num8 = input.nextDouble();
+        double max = Math.max(Math.max(num6, num7), num8);
+        double min = Math.min(Math.min(num6, num7), num8);
+        System.out.println(max + "\n" + min);
 	}
 
 }
+
